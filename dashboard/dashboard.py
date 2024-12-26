c432c8cd72123d08506077e97879d61d8cfd72bb
Dirga <115365051+Holylight5@users.noreply.github.com>
2024-12-26 15:03:17 +0700

    dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -34,7 +34,7 @@ if analysis_number == 1:
         ### No 1
         Apakah bulan dengan banyak hari libur memiliki lebih banyak penggunaan sepeda?
 
-        Dilihat dari hasil visualisasi data di atas, banyaknya hari libur tidak menentukan banyaknya pengguna sepeda pada bulan tersebut.
+        Dilihat dari hasil visualisasi data di bawah, banyaknya hari libur tidak menentukan banyaknya pengguna sepeda pada bulan tersebut.
         """
     )
     # Menggunakan plot dari file dataHolidayWorkingday
@@ -46,7 +46,7 @@ elif analysis_number == 2:
         ### No 2
         Apakah pengguna sepeda lebih terpengaruh oleh suhu atau kecepatan angin?
 
-        Berdasarkan diagram di atas dapat disimpulkan bahwa suhu dan kecepatan angin berpengaruh pada banyaknya pengguna sepeda. Pada bagian suhu, semakin nilai suhunya kecil maka pengguna sepeda sedikit. Sementara pada kecepatan angin, pengguna sepeda tidak terlalu berpengaruh besar dengan banyaknya pengguna sepeda.
+        Berdasarkan diagram di bawah dapat disimpulkan bahwa suhu dan kecepatan angin berpengaruh pada banyaknya pengguna sepeda. Pada bagian suhu, semakin nilai suhunya kecil maka pengguna sepeda sedikit. Sementara pada kecepatan angin, pengguna sepeda tidak terlalu berpengaruh besar dengan banyaknya pengguna sepeda.
         """
     )
     # Menggunakan plot dari file datatemp
@@ -58,7 +58,7 @@ elif analysis_number == 3:
         ### No 3
         Diantara keduanya mana yang paling banyak menggunakan sepeda, pengguna terdaftar atau tidak terdaftar?
 
-        Dari data di atas dapat disimpulkan bahwa pengguna paling banyak didapatkan oleh pengguna yang terdaftar dengan total pengguna lebih dari 2,5jt.
+        Dari data di bawah dapat disimpulkan bahwa pengguna paling banyak didapatkan oleh pengguna yang terdaftar dengan total pengguna lebih dari 2,5jt.
         """
     )
     # Menggunakan plot dari file datapengguna
@@ -70,7 +70,7 @@ elif analysis_number == 4:
         ### No 4
         Di musim apa sepeda paling banyak di rental?
 
-        Dari data di atas menunjukkan bahwa pengguna sepeda paling banyak pada musim gugur, bisa jadi karena cuaca di musim gugur yang sejuk dengan pemandangan gugurnya daun pohon yang membuat kesan romantis.
+        Dari data di bawah menunjukkan bahwa pengguna sepeda paling banyak pada musim gugur, bisa jadi karena cuaca di musim gugur yang sejuk dengan pemandangan gugurnya daun pohon yang membuat kesan romantis.
         """
     )
     # Menggunakan plot dari file datamusim
