1924c9a11bac1f6f029c34bc1f1ef8cd75dd81cd
Dirga <115365051+Holylight5@users.noreply.github.com>
2024-12-26 15:02:11 +0700

    dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -21,50 +21,57 @@ with st.sidebar:
         """
     )
 
-# No 1 Apakah bulan dengan banyak hari libur memiliki lebih banyak penggunaan sepeda
-st.write(
-    """
-    ### No 1
-    Apakah bulan dengan banyak hari libur memiliki lebih banyak penggunaan sepeda
-
-   Dilihat dari hasil visualisasi data diatas, banyaknya hari libur tidak menentukan banyaknya pengguna sepeda pada bulan tersebut.
-    """
+# Menambahkan filter untuk memilih No yang ingin ditampilkan
+analysis_number = st.sidebar.selectbox(
+    "Pilih Analisis",
+    [1, 2, 3, 4],  # Pilihan analisis
+    index=0  # Default pilihan
 )
-# Menggunakan plot dari file datapengguna
-st.pyplot(dataHolidayWorkingday.fig)
 
-#Apakah pengguna sepeda lebih terpengaruh oleh suhu atau kecepatan angin?
-st.write(
-    """
-    ### No 2
-    Apakah pengguna sepeda lebih terpengaruh oleh suhu atau kecepatan angin?
+if analysis_number == 1:
+    st.write(
+        """
+        ### No 1
+        Apakah bulan dengan banyak hari libur memiliki lebih banyak penggunaan sepeda?
 
-    Bedasarkan diagram diatas dapat disimpulkan bahwa suhu dan kecepatan angin berpengaruh pada banyaknya pengguna sepeda. Pada bagian suhu semakin nilai suhunya kecil maka pengguna sepeda sedikit. Sementara pada kecepatan angin pengguna sepeda tidak terlalu berpengaruh besar dengan banyaknya pengguna sepeda.
-    """
-)
-# Menggunakan plot dari file datatemp
-st.pyplot(datatemp.fig)
+        Dilihat dari hasil visualisasi data di atas, banyaknya hari libur tidak menentukan banyaknya pengguna sepeda pada bulan tersebut.
+        """
+    )
+    # Menggunakan plot dari file dataHolidayWorkingday
+    st.pyplot(dataHolidayWorkingday.fig)
 
-# No 3 Diantara keduanya mana yang paling banyak menggunakan sepeda, pengguna terdaftar atau tidak terdaftar?
-st.write(
-    """
-    ### No 3
-    Yang paling banyak menggunakan sepeda, pengguna terdaftar atau tidak terdaftar?
+elif analysis_number == 2:
+    st.write(
+        """
+        ### No 2
+        Apakah pengguna sepeda lebih terpengaruh oleh suhu atau kecepatan angin?
 
-    Dari data diatas dapat disimpulkan bahwa pengguna paling banyak didapatkan oleh pengguna yang terdaftar dengan total pengguna lebih dari 2,5jt.
-    """
-)
-# Menggunakan plot dari file datamusim
-st.pyplot(datapengguna.fig)
+        Berdasarkan diagram di atas dapat disimpulkan bahwa suhu dan kecepatan angin berpengaruh pada banyaknya pengguna sepeda. Pada bagian suhu, semakin nilai suhunya kecil maka pengguna sepeda sedikit. Sementara pada kecepatan angin, pengguna sepeda tidak terlalu berpengaruh besar dengan banyaknya pengguna sepeda.
+        """
+    )
+    # Menggunakan plot dari file datatemp
+    st.pyplot(datatemp.fig)
 
-# No 4 Di musim apa sepeda paling banyak di rental?
-st.write(
-    """
-    ### No 4
-    Di musim apa sepeda paling banyak di rental?
+elif analysis_number == 3:
+    st.write(
+        """
+        ### No 3
+        Diantara keduanya mana yang paling banyak menggunakan sepeda, pengguna terdaftar atau tidak terdaftar?
 
-    Dari data diatas menunjukan bahwa pengguna sepeda paling banyak pada musim gugur, bisa jadi karena cuaca di musim gugur yang sejuk dengan pemandangan gugurnya daun pohon yang membuat kesan romantis.
-    """
-)
-# Menggunakan plot dari file dataHolidayWorkingday
-st.pyplot(datamusim.fig)
+        Dari data di atas dapat disimpulkan bahwa pengguna paling banyak didapatkan oleh pengguna yang terdaftar dengan total pengguna lebih dari 2,5jt.
+        """
+    )
+    # Menggunakan plot dari file datapengguna
+    st.pyplot(datapengguna.fig)
+
+elif analysis_number == 4:
+    st.write(
+        """
+        ### No 4
+        Di musim apa sepeda paling banyak di rental?
+
+        Dari data di atas menunjukkan bahwa pengguna sepeda paling banyak pada musim gugur, bisa jadi karena cuaca di musim gugur yang sejuk dengan pemandangan gugurnya daun pohon yang membuat kesan romantis.
+        """
+    )
+    # Menggunakan plot dari file datamusim
+    st.pyplot(datamusim.fig)
